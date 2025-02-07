badb91ecdfd2c5d4afd3b3dc7275dc4088913aa8
TristanDekeyser <tristan.dekeyser@gmail.com>
2025-02-07 15:03:54 +0100

    Update module

--- library/module.py
+++ library/module.py
@@ -13,19 +13,9 @@ def main():
         # Controleer of het commando succesvol was
         if result.returncode == 0:
             # Stuur de standaard uitvoer terug (als string)
-            response = {
-                "message": "De ls opdracht is geslaagd.",
-                "output": result.stdout.strip()  # Verwijder extra nieuwe regels
-            }
-            # Gebruik module.exit_json om de output correct te returnen
             module.exit_json(changed=False, msg="De ls opdracht is geslaagd", output=result.stdout.strip())
         else:
             # Als er een fout is bij het uitvoeren van de 'ls' opdracht
-            response = {
-                "message": "Er is een fout opgetreden bij het uitvoeren van 'ls'.",
-                "error": result.stderr.strip()  # Stuur de foutmelding als string
-            }
-            # Gebruik module.fail_json om foutinformatie door te geven
             module.fail_json(msg="Fout bij het uitvoeren van 'ls'.", error=result.stderr.strip())
 
     except Exception as e:
