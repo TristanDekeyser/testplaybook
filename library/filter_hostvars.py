6f7bc6d3dfaff1a926cb869e1acad32d2ba9f808
TristanDekeyser <tristan.dekeyser@gmail.com>
2025-02-11 13:20:34 +0100

    Update filter_hostvars

--- library/filter_hostvars.py
+++ library/filter_hostvars.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
-import json
+import yaml
 from ansible.module_utils.basic import AnsibleModule
 
 def main():
     # Definieer de AnsibleModule met eventuele parameters
     module = AnsibleModule(
         argument_spec={
-            'hostvars_json': {'type': 'str', 'required': True}
+            'vars_file': {'type': 'str', 'required': True}
         }
     )
 
     try:
-        # Laad de hostvars uit de input JSON
-        hostvars = json.loads(module.params['hostvars_json'])
+        # Lees de inhoud van de vars.yml file
+        with open(module.params['vars_file'], 'r') as f:
+            vars_data = yaml.safe_load(f)
 
-        # Hier filteren we alleen de benodigde hostvariabelen
-        # In dit geval, halen we een paar specifieke variabelen uit, zoals 'semaphore_vars'
-        filtered_vars = {}
-        for host, vars in hostvars.items():
-            if 'semaphore_vars' in vars:
-                filtered_vars[host] = vars['semaphore_vars']
-        
-        # Stuur de gefilterde variabelen terug
-        module.exit_json(changed=False, filtered_vars=filtered_vars)
+        # Hier sturen we de inhoud van de vars.yml terug
+        module.exit_json(changed=False, vars_data=vars_data)
 
     except Exception as e:
         # Foutafhandelingsblok voor onverwachte fouten
