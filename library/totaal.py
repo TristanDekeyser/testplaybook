6cbae71705484a53fb9631ab7ec76fa87f652669
TristanDekeyser <tristan.dekeyser@gmail.com>
2025-02-20 13:51:50 +0100

    Update totaal.py

--- library/totaal.py
+++ library/totaal.py
@@ -4,8 +4,8 @@ import json
 from ansible.module_utils.basic import AnsibleModule
 
 # Semaphore API Configuratie
-SEMAPHORE_URL = "http://192.168.242.133:3000/api"
-SEMAPHORE_TOKEN = "owozuup-zne7p-stkhhs3hdfr6efiyk1rh8okh_70bu="
+SEMAPHORE_URL = "http://192.168.242.160:3000/api"
+SEMAPHORE_TOKEN = "tuwnyelwoulicyww1e87zznnnaq7w6pkoadjh-z3uz8="
 PROJECT_ID = 1
 
 HEADERS = {
@@ -29,8 +29,8 @@ def create_inventory(module, hosts):
         "name": "Backup Inventory",
         "project_id": PROJECT_ID,
         "inventory": inventory_yaml,
-        "ssh_key_id": 2,
-        "become_key_id": 4,
+        "ssh_key_id": 1,
+        "become_key_id": 2,
         "type": "static-yaml"
     }
 
@@ -91,8 +91,8 @@ def main():
     if not playbook_name:
         module.fail_json(msg="Geen playbook naam gevonden!")
 
-    inventory_id = 16 if not failed_hosts else create_inventory(module, failed_hosts)
-    environment_id = 2 if not failed_vars else create_environment(module, failed_vars)
+    inventory_id = 2 if not failed_hosts else create_inventory(module, failed_hosts)
+    environment_id = 1 if not failed_vars else create_environment(module, failed_vars)
 
     if create_template(module, inventory_id, environment_id, playbook_name):
         module.exit_json(changed=True, msg="Semaphore resources succesvol aangemaakt!")
